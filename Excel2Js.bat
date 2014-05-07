1b7bcfe501a61e8042b87b4e4f7b952e9a0cf3a5
zhaozuod3 <zhzuodong@gmail.com>
2014-05-07 11:31:50 +0800

    + batch

--- Excel2Js.bat
+++ Excel2Js.bat
@@ -2,5 +2,8 @@
 rem init node environment
 call "C:\Program Files\nodejs\nodevars.bat"
 
+node ./convertor/x2j.js ./table/items.js ./items.xlsx
+node ./convertor/x2j.js ./table/roles.js ./roles.xlsx
+
 echo OK.
 pause
--- Js2Excel.bat
+++ Js2Excel.bat
@@ -2,7 +2,8 @@
 rem init node environment
 call "C:\Program Files\nodejs\nodevars.bat"
 
-echo %cd%
+node ./convertor/j2x.js ./items.xlsx ../table/items.js
+node ./convertor/j2x.js ./roles.xlsx ../table/roles.js
 
 echo OK.
 pause
