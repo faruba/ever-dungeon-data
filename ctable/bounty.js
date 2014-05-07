02bd22e7580ee51a48c636bfc441e94392de9ebb
Ex-Bob <shabeasanbob@gmail.com>
2014-05-07 11:27:40 +0800

    活动

---
+++ ctable/bounty.js
@@ -0,0 +1,34 @@
+exports.data = [
+    {
+        "bountyId":0,
+        "title":"捉拿土豪哥布林",
+        "desc":"哥布林中的土豪出现了！趁他们逃跑之前击倒，获得大量金币！",
+        "count":3,
+        "date":{
+            "segment":[
+                {
+                    "start":"11:00",
+                    "end":"23:59"
+                }
+            ]
+        },
+        "begin":1,
+        "tittlePic":"bounty-icon3.png",
+        "timePic":"bounty-time3.png",
+         "level":[
+            {
+                "powerLimit":100,
+                "stage":"114"
+
+            },
+            {
+                "powerLimit":200,
+                "stage":"115"
+            },
+            {
+                "powerLimit":400,
+                "stage":"116"
+            }
+        ]
+    }
+]
