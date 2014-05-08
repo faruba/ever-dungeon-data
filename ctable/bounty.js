a93241cb4f890ccec95da3b24f3cba5b94728a62
Ex-Bob <shabeasanbob@gmail.com>
2014-05-08 16:04:47 +0800

    哥布林修改

--- ctable/bounty.js
+++ ctable/bounty.js
@@ -7,7 +7,7 @@ exports.data = [
         "date":{
             "segment":[
                 {
-                    "start":"11:00",
+                    "start":"12:00",
                     "end":"23:59"
                 }
             ]
