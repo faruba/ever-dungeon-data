ff0ed5c2a4294e80527bdaf58b750eecb330505f
Ex-Bob <shabeasanbob@gmail.com>
2014-05-13 09:48:12 +0800

    强化

--- Excel2Js.bat
+++ Excel2Js.bat
@@ -4,6 +4,7 @@ call "C:\Program Files\nodejs\nodevars.bat"
 
 node ./convertor/x2j.js ./table/items.js ./items.xlsx
 node ./convertor/x2j.js ./table/roles.js ./roles.xlsx
+node ./convertor/x2j.js ./table/enhance.js ./enhance.xlsx
 
 echo OK.
 pause
--- __Js2Excel.bat
+++ __Js2Excel.bat
@@ -4,6 +4,7 @@ call "C:\Program Files\nodejs\nodevars.bat"
 
 node ./convertor/j2x.js ./items.xlsx ../table/items.js
 node ./convertor/j2x.js ./roles.xlsx ../table/roles.js
+node ./convertor/j2x.js ./enhance.xlsx ../table/enhance.js 2
 
 echo OK.
 pause
