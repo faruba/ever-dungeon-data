54b85e08ebe20d0a61816ae8b2ce736b711fdb41
lava-hammer <lava_hammer@163.com>
2014-05-13 16:09:21 +0800

    fix x2j

--- convertor/x2j.js
+++ convertor/x2j.js
@@ -2,44 +2,53 @@ var fs = require("fs");
 var xlsx = require("node-xlsx");
 
 function linkObject(target, value, path, type){
-	//console.log("- link("+value+", "+path+", "+typeof(value)+")");
 	if( value == null ){
 		return;
 	}
 	var parent = target;
 	var lastKey = null;
 	var lastParent = parent;
-	var compos = path.split(".");
-	compos = compos.filter(function(str){
-		if( str == '' ) return false;
-		return true;
-	});
+	var compos = [];
+	var buffer = "";
+	path = path + ".";
+	for(var k in path){
+		var c = path[k];
+		if( c == "." || c == "[" || c == "]" ){
+			if( buffer != "" ){
+				var val = Number(buffer);
+				if( val == val ){//NaN
+					compos.push(val);
+				}
+				else{
+					compos.push(buffer);
+				}
+				buffer = "";
+			}
+		}
+		else{
+			buffer += c;
+		}
+	}
 	for(var k in compos){
 		var key = compos[k];
-		var isArray = false;
-		var index = -1;
-		if( key[key.length-1] == "]" ){
-			isArray = true;
-			index = Number(key.substring(key.indexOf("[")+1, key.length-1));
-			key = key.substring(0, key.indexOf("["));
-		}
 		lastKey = key;
 		lastParent = parent;
-		//console.log("* key = "+key+" ("+isArray+")");
 		if( parent[key] == null ){
-			if( isArray ){
-				parent[key] = [];
-				//console.log("+ "+key+": array");
-				lastParent = parent[key];
-				lastKey = index;
-				parent[key][index] = {};
-				parent = parent[key][index];
+			if( k < compos.length-1 ){
+				if( typeof(compos[Number(k)+1])=="number" ){
+					console.log("+ "+key+"[]");
+					parent[key] = [];
+					lastParent = parent[key];
+					lastKey = key;
+					parent = parent[key];
+				}
+				else{
+					console.log("+ "+key);
+					parent[key] = {};
+					parent = parent[key];
+				}
 			}
-			else{
-				parent[key] = {};
-				//console.log("+ "+key+": object");
-				parent = parent[key];
-			}
+			
 		}
 		else{
 			parent = parent[key];
