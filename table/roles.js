a0c4e2bdf708b69856d580bd7262004a5c1ffab9
Ex-Bob <shabeasanbob@gmail.com>
2014-06-10 16:46:59 +0800

    transid

--- table/roles.js
+++ table/roles.js
@@ -30,7 +30,8 @@ exports.data = [
             662,
             712
 		],
-		"levelId": 0
+		"levelId": 0,
+        "transId":197
 	},
 	{
 		"classId": 1,
@@ -63,7 +64,9 @@ exports.data = [
 			652,
             742
 		],
-		"levelId": 1
+		"levelId": 1,
+        "transId":198
+
 	},
 	{
 		"classId": 2,
@@ -96,7 +99,8 @@ exports.data = [
             682,
             772
 		],
-		"levelId": 2
+		"levelId": 2,
+        "transId":199
 	},
 	{
 		"classId": 3,
