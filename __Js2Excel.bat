963f63a5dd49ff03a859f7df1bab9ac8f9299ea4
Ex-Bob <shabeasanbob@gmail.com>
2014-07-07 14:48:34 +0800

    修正

--- __Js2Excel.bat
+++ __Js2Excel.bat
@@ -2,8 +2,7 @@
 rem init node environment
 call "C:\Program Files\nodejs\nodevars.bat"
 
-node ./convertor/j2x.js ./items.xlsx ../table/items.js 2
-node ./convertor/j2x.js ./enhance.xlsx ../table/enhance.js 2
+node ./convertor/j2x.js ./level.xlsx ../table/level.js 2
 
 echo OK.
 pause
