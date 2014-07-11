81e8e3f9030ff106aa97eb3030670a4494d86bb2
Ex-Bob <shabeasanbob@gmail.com>
2014-07-11 18:02:37 +0800

    版本修改

--- stable/version.js
+++ stable/version.js
@@ -3,7 +3,7 @@ exports.data = {
           "quest": "0.0.4",
           "stage": "0.1.5",
           "bin_version": "1.0.7",
-          "bin_version_need": "1.0.4",
+          "bin_version_need": "1.0.6",
           "url": "http://hotupdate.qiniudn.com",
           "bin_url": "http://fir.im/rX4"
 }
