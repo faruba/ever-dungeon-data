2796cfb55dbf556416cb2e17139db9f402a0858f
zhouqiangqiang <wowwow0@163.com>
2014-08-04 15:34:57 +0800

    增加测试怪

--- table/dungeons.js
+++ table/dungeons.js
@@ -38631,7 +38631,12 @@ exports.data = [
                     {"pool":"soldierPool","from":8,to:14},
                     {"pool":"rangePool","from":0,to:1},
                     {"pool":"elitePool","count":1},
-                    {"id": 149,"from":0,to:2}
+                    {"id": 149,"from":0,to:2},
+                    {
+                        "id": 215,
+                        "count":1,
+                        "keyed":true
+                    }
                 ]
             },
             {
