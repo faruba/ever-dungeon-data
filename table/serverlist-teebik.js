f970bc29c871456acd0ce794d91584efbd17f078
ijovi23 <ijovi23@gmail.com>
2014-09-26 12:36:51 +0800

    new table : server list

---
+++ table/serverlist-teebik.js
@@ -0,0 +1,7 @@
+exports.data = [
+    {
+        "name": "master",
+        "ip" : "148.251.128.11",
+        "port": 7757
+    }
+];
---
+++ table/serverlist-trin.js
@@ -0,0 +1,12 @@
+exports.data = [
+    {
+        "name": "master",
+        "ip" : "60.191.205.25",
+        "port": 7757
+    },
+    {
+        "name": "develop",
+        "ip" : "122.226.199.14",
+        "port": 7757
+    }
+];
