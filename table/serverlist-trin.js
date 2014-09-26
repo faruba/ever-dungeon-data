21bde4102da76cf27667306eecc747ac1de88342
ijovi23 <ijovi23@gmail.com>
2014-09-26 15:20:44 +0800

    no message

--- table/serverlist-trin.js
+++ table/serverlist-trin.js
@@ -1,10 +1,10 @@
 exports.data = [
     {
         "name": "develop",
-        "address":["122.226.199.14",7757]
+        "address":{ip:"122.226.199.14", port:7757}
     },
     {
         "name": "master",
-        "address":["60.191.205.25",7757]
+        "address":{ip:"60.191.205.25", port:7757}
     }
 ];
