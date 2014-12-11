12fb5a4610c401d5f3888a1a3128f72e4215ff69
faruba <farubafaruba@126.com>
2014-12-11 12:20:55 +0800

    pkreward

--- stable/pkReward.js
+++ stable/pkReward.js
@@ -2,7 +2,29 @@ exports.data = [
     {
         top:5,
         prize: [
+            {"type":1, "count":150}
+        ]
+    },
+    {
+        top:15,
+        prize: [
             {"type":1, "count":50}
         ]
     },
+    {
+        top:55,
+        prize: [
+            {"type":1, "count":50}
+        ]
+    },
+	{
+		top: Infinity,
+		prize: [
+            {"type":1, "count":50}
+        ]
+	},
 ];
+
+
+//6->5 =>50
+//6->4 =>200
