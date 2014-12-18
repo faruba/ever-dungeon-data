b3c404daa7bdbbf2126a540ddb1f757e1e4ab14a
faruba <farubafaruba@126.com>
2014-12-18 11:29:59 +0800

    version

--- stable/version.js
+++ stable/version.js
@@ -5,5 +5,9 @@ exports.data = {
           "bin_version": "1.0.10",
           "bin_version_need": "1.0.10",
           "url": "http://hotupdate.qiniudn.com",
-          "bin_url": ""
+          "bin_url": "",
+		  "needed_version":"1.1.26",
+          "last_version":"1.1.26",
+          "suggest_version":"1.1.26"
 }
+
