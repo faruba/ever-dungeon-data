974c53dcb45e9d586be0dbdcb56895b5ed99dec6
tringame <tringame@tringamedeMac-mini.local>
2014-12-25 15:08:15 +0800

    套装属性

---
+++ table/unit.js
@@ -0,0 +1,9 @@
+exports.data = [
+    {//0
+        'suitId':0,
+        '2':[{type: 'incress_property', property: { attack: 10 } } ],
+        '4':[{type: 'incress_property', property: { attack: 10 } },
+             {type: 'change_appearance', appearance: { head: 10 } },
+             {type: 'install_skill', id: 3, level: 3 }]
+    }
+]
