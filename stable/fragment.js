1949620c0e40abe65751f1ea55739e74d8dd36dc
tringame <tringame@tringamedeMac-mini.local>
2014-12-25 12:35:11 +0800

    新增抽奖的表

---
+++ stable/fragment.js
@@ -0,0 +1,156 @@
+exports.data = [
+    {//0
+        "interval":{"value":5,"unit":"second"},//second minite hour day month year
+        "basic_times":3,
+        "basic_prize":[
+            {
+                "rate": 1,
+                "prize": [
+                    {
+                        "weight": 18,
+                        "type": 0,
+                        "value":810 ,
+                        "count": 1
+                    },
+                    {
+                        "weight": 18,
+                        "type": 0,
+                        "value": 811,
+                        "count": 1
+                    },
+                    {
+                        "weight": 10,
+                        "type": 0,
+                        "value": 812,
+                        "count":1
+                    },
+                    {
+                        "weight": 18,
+                        "type": 0,
+                        "value": 1475,
+                        "count":1
+                    }
+                ]
+            },
+            {
+                "rate": 0.1,
+                "prize": [
+                    {
+                        "weight": 18,
+                        "type": 0,
+                        "value":813,
+                        "count": 1
+                    },
+                    {
+                        "weight": 6,
+                        "type": 0,
+                        "value": 814,
+                        "count": 2
+                    },
+                    {
+                        "weight": 2,
+                        "type": 0,
+                        "value": 815,
+                        "count":3
+                    }
+                ]
+            }
+        ],
+        "advanced_prize":[
+            {
+                "rate": 1,
+                "prize": [
+                    {
+                        "weight": 18,
+                        "type": 1,
+                        "count": 1
+                    }
+                ]
+            },
+            {
+                "rate": 0.08,
+                "prize": [
+                    {
+                        "weight": 18,
+                        "type": 1,
+                        "count": 2
+                    }
+                ]
+            }
+        ]
+    },
+    { //1
+        "interval":{"value":10,"unit":"second"},
+        "basic_times":3,
+        "basic_prize":[
+            {
+                "rate": 1,
+                "prize": [
+                    {
+                        "weight": 18,
+                        "type": 0,
+                        "value":824 ,
+                       "count": 1
+                    },
+                    {
+                        "weight": 18,
+                        "type": 0,
+                        "value": 825,
+                        "count": 1
+                    },
+                    {
+                        "weight": 10,
+                        "type": 0,
+                        "value": 826,
+                        "count":1
+                    }
+                ]
+            },
+            {
+                "rate": 0.1,
+                "prize": [
+                    {
+                        "weight": 18,
+                        "type": 0,
+                        "value":837 ,
+                        "count": 1
+                    },
+                    {
+                        "weight": 6,
+                        "type": 0,
+                        "value": 838,
+                        "count": 2
+                    },
+                    {
+                        "weight": 2,
+                        "type": 0,
+                        "value": 839,
+                        "count":3
+                    }
+                ]
+            }
+        ],
+        "advanced_prize":[
+            {
+                "rate": 1,
+                "prize": [
+                    {
+                        "weight": 18,
+                        "type": 2,
+                        "count": 1
+                    }
+                ]
+            },
+            {
+                "rate": 0.08,
+                "prize": [
+                    {
+                        "weight": 18,
+                        "type": 2,
+                        "count": 2
+                    }
+                ]
+            }
+        ]
+    }
+]
