9add8ac80079cc49ba84cd5132979099dbf9ff7a
Kun <nukoug@gmail.com>
2015-01-06 22:24:23 +0800

    mutiple server

--- stable/config-multi-teebik.js
+++ stable/config-multi-teebik.js
@@ -1,53 +1,31 @@
 exports.data = {
-  "ServerID": 0,
-  "ServerName": "Develop",
-  "ServerConfig": [
-    {
-      "ID": 0,
-      "Name": "Develop",
-      "DB": {
-        "Account": { "IP": "192.168.128.9", "PORT": 6380 },
-        "Role": { "IP": "192.168.128.10", "PORT": 6380 },
-        "Publisher": { "IP": "192.168.128.9", "PORT": 6380 },
-        "Subscriber": { "IP": "192.168.128.9", "PORT": 6380 }
-      },
-      "Gate": [
-        {"ip": "192.168.128.9", "port": 7756},
-        {"ip": "192.168.128.10", "port": 7756},
-        {"ip": "192.168.128.11", "port": 7756}
-      ]
+    "Auth_API_Server": "192.168.128.11"
+
+    DB_Config: {
+        "SEAsia": {
+            "Account": { "IP": "192.168.128.9", "PORT": 6380 },
+            "Role": { "IP": "192.168.128.10", "PORT": 6380 },
+            "Publisher": { "IP": "192.168.128.9", "PORT": 6380 },
+            "Subscriber": { "IP": "192.168.128.9", "PORT": 6380 }
+        }
+    },
+
+    Server_Config: {
+        "Develop": {
+            ID: 0,
+            Name: "Develop",
+            DB: "SEAsia",
+            IP: "10.4.3.41",
+            DB_Prefix: "Develop"
+         "Gate": [
+           {"ip": "192.168.128.9", "port": 7756},
+           {"ip": "192.168.128.10", "port": 7756},
+           {"ip": "192.168.128.11", "port": 7756}
+         ]
+        }
+    },
+
+    IP_Config : {
+        "192.168.128.9": [ { Server: "Develop", Port: 7757 } ],
     }
-
-  ],
-  "PayMethod": "PP",
-  "Enhance_Rate" : [1, 0.75, 0.70, 0.65, 0.6, 0.55, 0.50, 0.45, 0.40, 0.35],
-  "Product_List": [
-    {"rmb":0.99,"diamond":120},
-    {"rmb":1.99,"diamond":258},
-    {"rmb":4.99,"diamond":650},
-    {"rmb":9.99,"diamond":1310},
-    {"rmb":19.99,"diamond":2650},
-    {"rmb":29.99,"diamond":4340},
-    {"rmb":49.99,"diamond":7400},
-    {"rmb":99.99,"diamond":15420},
-    {"rmb":3.99,"diamond":0}
-  ],
-  "Global_Blue_Star_RewardTable" : [ ],
-  "Global_Recyclable_Enhancement" : [ 0, 1, 2, 3, 4, 5, 6 ],
-  "Global_Recycle_Config" : [
-    {"minimum" : 0, "delta" : 1},
-    {"minimum" : 1, "delta" : 1},
-    {"minimum" : 2, "delta" : 2},
-    {"minimum" : 3, "delta" : 3},
-    {"minimum" : 4, "delta" : 4},
-    {"minimum" : 5, "delta" : 5},
-    {"minimum" : 6, "delta" : 6},
-    {"minimum" : 7, "delta" : 7},
-    {"minimum" : 8, "delta" : 8},
-    {"minimum" : 9, "delta" : 9}
-  ],
-
-    "Global_Material_ID" : [558, 559, 560, 561, 562],
-    "Global_Enhancement_GEM_Index" : [3, 4, 5, 26, 27, 392, 393],
-  "Auth_API_Server": "192.168.128.11"
 }
