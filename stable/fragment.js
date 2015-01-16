2fd8cf21e9569462ef18fa19fd3cddcd3ae1adb2
zhouqiangqiang <wowwow0@163.com>
2015-01-16 18:07:29 +0800

    抽宝箱时间

--- stable/fragment.js
+++ stable/fragment.js
@@ -1,6 +1,6 @@
 exports.data = [
     {//0
-        "interval":{"value":5,"unit":"second"},//second minite hour day month year
+        "interval":{"value":30,"unit":"second"},//second minite hour day month year
         "basic_times":3,
         "basic_prize":[
             {
