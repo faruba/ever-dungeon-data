3fd572a2be5803eb14150645b7d5e1cc31e39ed1
faruba <farubafaruba@126.com>
2015-03-27 11:29:42 +0800

    prentice table

---
+++ table/prentices.js
@@ -0,0 +1,17 @@
+exports.data = {
+    'globalCfg':{
+        maxPrentice:2,
+        unlockPrenticeCond:function(){},
+        
+    },
+    '0':{
+        // [level_n_cost,...]
+        upgradeCost:[],
+        unlockSkill:[
+            [0,1,2], // level_1
+            [10,11,21], // level_1
+            ],
+        initialEquip:[1,2,3],
+    },
+
+}
