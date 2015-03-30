e330d5a65524d96eba3a9b3a9e04417ae56cb28d
faruba <farubafaruba@126.com>
2015-03-30 11:11:58 +0800

    prentices

--- table/prentices.js
+++ table/prentices.js
@@ -1,17 +1,40 @@
 exports.data = {
     'globalCfg':{
         maxPrentice:2,
-        unlockPrenticeCond:function(){},
+        unlockCond:function(){},
+        unlockCost:{
+            //current_prentice_count:costid
+            1:11,
+        },
+        rebornCost:3
         
     },
     '0':{
-        // [level_n_cost,...]
-        upgradeCost:[],
+        // [ level_n_cost,...]
+        upgradeCost:[1,2],
         unlockSkill:[
             [0,1,2], // level_1
             [10,11,21], // level_1
             ],
-        initialEquip:[1,2,3],
+        initialEquip:[43, 42, 41, 712, 40, 622],
+    },
+    '1':{
+        // [ level_n_cost,...]
+        upgradeCost:[1,2],
+        unlockSkill:[
+            [0,1,2], // level_1
+            [10,11,21], // level_1
+            ],
+        initialEquip:[35, 34, 33, 742, 32, 652],
+    },
+    '2':{
+        // [ level_n_cost,...]
+        upgradeCost:[1,2],
+        unlockSkill:[
+            [0,1,2], // level_1
+            [10,11,21], // level_1
+            ],
+        initialEquip:[39, 38, 37, 772, 36, 682],
     },
 
 }
